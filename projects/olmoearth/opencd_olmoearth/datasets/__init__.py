295151e54630e606c6040987bc3a1cc36b9599b8
JayWang <62465258+wangjieh@users.noreply.github.com>
2026-06-08 16:27:14 +0800

    Add files via upload

--- projects/olmoearth/opencd_olmoearth/datasets/__init__.py
+++ projects/olmoearth/opencd_olmoearth/datasets/__init__.py
@@ -1,4 +1,13 @@
 from .bright import OLMoEarthBRIGHTDataset
+from .levir_cd import (
+    LoadOLMoEarthLEVIRAnnotations,
+    LoadOLMoEarthLEVIRPair,
+    OLMoEarthLEVIRCDDataset,
+)
 
-__all__ = ["OLMoEarthBRIGHTDataset"]
-
+__all__ = [
+    "OLMoEarthBRIGHTDataset",
+    "OLMoEarthLEVIRCDDataset",
+    "LoadOLMoEarthLEVIRPair",
+    "LoadOLMoEarthLEVIRAnnotations",
+]
