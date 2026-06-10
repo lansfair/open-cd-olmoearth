5543233474ee5954db8f9a0d2fe81d0679f3b4c7
JayWang <62465258+wangjieh@users.noreply.github.com>
2026-06-10 09:39:51 +0800

    Update __init__.py

--- opencd/engine/hooks/__init__.py
+++ opencd/engine/hooks/__init__.py
@@ -1,4 +1,5 @@
 # Copyright (c) Open-CD. All rights reserved.
 from .visualization_hook import CDVisualizationHook
+from .visualization_hook_Jie import CDVisualizationHook_Jie
 
-__all__ = ['CDVisualizationHook']
+__all__ = ['CDVisualizationHook', 'CDVisualizationHook_Jie']
